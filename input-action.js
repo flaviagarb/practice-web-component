ec451beb5c8aea1d7edfd8a8128cb3cc8a7d30b9
Flavia Garbetta <flavia.garbetta@gmail.com>
2025-05-08 11:32:03 +0200

    Refine

--- input-action.js
+++ input-action.js
@@ -1,5 +1,5 @@
 const template = document.createElement("template");
-// Paso 1: Crear el template que tiene mi componente
+
 template.innerHTML = `
   <div class="kc-component">
     <input type="text" (/>
@@ -8,7 +8,7 @@ template.innerHTML = `
 `
 
 class InputAction extends HTMLElement {
-  // Paso 2: Añadir atributos que recibo de fuera al componente
+
   constructor() {
     super();
 
@@ -21,7 +21,7 @@ class InputAction extends HTMLElement {
 
   connectedCallback() {
     const templateCopy = template.content.cloneNode(true);
-    // Paso 3: Incluimos los 2 valores anteriores dentro del DOM
+
 
     const input = templateCopy.querySelector("input");
     const button = templateCopy.querySelector("button");
@@ -29,30 +29,27 @@ class InputAction extends HTMLElement {
     input.setAttribute("placeholder", this.placeholder);
     button.textContent = this.buttonLabel;
 
-    // Paso 4: recoger el texto que se escribe cuando se pula el boton "+" > por lo tanto añadir evento que escucha click, ver que valor tiene y disparar el evento
+
     button.addEventListener("click", () => {
-      // coger lo que tiene input
+
       const inputValue = input.value;
-      // creamos el evento con este valor y lo mandamos. Los párametros de CustomEvent son el nombre del evento y la propiedad DETAIL que añade el inputValue
+
       const event = new CustomEvent("input-action-submit", {
         detail: inputValue
       })
-      // lanzamos el evento sobre el nodo del DOM
+
       this.dispatchEvent(event)
-      //limpiar el input después de que el boton se pulse
+
       input.value = ""
       button.setAttribute("disabled", "")
     })
-    // Paso 5: mantener el botón deshabilitado mientras no haya nada escrito. Escuchar cada vez que cambie
+
     input.addEventListener("input", () => {
-      // sacamos la referencia que ya tenemos arriba del valor
       const currentValue = input.value;
-      // si el input tiene valor, habilitar el botón
       if (currentValue) {
         button.removeAttribute("disabled")
-        // si el input está vacío, deshabilitar el botón
       } else {
-        button.setAttribute("disabled", "") // es un bool, hay que pasarle dos valores, el segundo siempre vacío
+        button.setAttribute("disabled", "")
       }
     })
 
